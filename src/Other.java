acebe88632b35a2ec3b81fa5a9129f7d00c4ed39
PriestofTuna <lyttekk@yahoo.com>
2015-11-11 21:35:55 -0500

    commit

---
+++ src/Other.java
@@ -0,0 +1,19 @@
+public class Other {
+
+	public static void main(String[] args) {
+		String tTuna = "Holy";
+		System.out.println(tTuna.charAt(1));
+		System.out.println(tTuna.concat(" Tuna"));
+		System.out.println(tTuna.equals("Holy"));
+		System.out.println(tTuna.equalsIgnoreCase("holy"));
+		System.out.println(tTuna.indexOf("Huh"));
+		System.out.println(tTuna.lastIndexOf("Pls"));
+		System.out.println(tTuna.toLowerCase());
+		System.out.println(tTuna.toUpperCase());
+		System.out.println(tTuna.replace('a','f'));
+		System.out.println(tTuna.substring(2));
+		System.out.println(tTuna.substring(1,3));
+		System.out.println(tTuna.trim());
+	}
+
+}
